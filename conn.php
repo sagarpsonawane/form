4ebed9b70114735995733ad4e70e0459503c609a
sagarpsonawane <54024700+sagarpsonawane@users.noreply.github.com>
2019-08-13 09:51:30 +0530

    Database credentials are change

--- conn.php
+++ conn.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'phpmyadmin');
-define('DB_PASSWORD', 'Sdfg@123');
-define('DB_NAME', 'phpmyadmin');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'info');
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
